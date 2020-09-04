6bd6133b00ec953878db0d1e57596e45fccd8a67
TheSawIsTheLaw <dimakrok@bk.ru>
2020-09-05 00:43:12 +0300

    ADD: starts of project and main UI

---
+++ AA_lab_01/mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.hpp"
+#include "ui_mainwindow.h"
+
+#include "QtDebug"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->label->setPixmap(QPixmap("../AA_lab_01/imgs/cyber.jpg"));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
