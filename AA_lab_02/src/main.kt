47a46b5939964dac2a0de66476f37fbdaba3b644
theSawIsTheLaw <dimakrok@bk.ru>
2020-10-03 15:37:24 +0300

    ADD: cols and rows computational for Vinograd

--- AA_lab_02/src/main.kt
+++ AA_lab_02/src/main.kt
@@ -7,7 +7,7 @@ import kotlin.reflect.typeOf
 
 fun printOutMatrix(matrix: Array<IntArray>)
 {
-    print("\n") 
+    print("\n")
     for (i in matrix.indices)
     {
         for (j in matrix[i].indices)
@@ -24,6 +24,28 @@ fun matricesMult(fMatrix: Array<IntArray>, sMatrix: Array<IntArray>) : Array<Int
             for (k in fMatrix[0].indices)
                 product[i][j] += fMatrix[i][k] * sMatrix[k][j]
     return product
+}
+
+fun rowsComputation(matrix: Array<IntArray>) : IntArray
+{
+    val computedRows = IntArray(matrix.size)
+
+    for (i in matrix.indices)
+        for (j in 0 .. (matrix[0].size - 1) / 2)
+            computedRows[i] += matrix[i][j * 2] * matrix[i][j * 2 + 1]
+
+    return computedRows
+}
+
+fun colsComputation(matrix: Array<IntArray>) : IntArray
+{
+    val computedCols = IntArray(matrix[0].size)
+
+    for (i in 0..((matrix.size - 1) / 2))
+        for (j in matrix[0].indices)
+            computedCols[j] += matrix[i * 2][j] * matrix[i * 2 + 1][j]
+
+    return computedCols
 }
 
 fun main()
