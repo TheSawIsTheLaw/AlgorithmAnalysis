45716db83c3a3de3519a180c5157ac6499b19416
theSawIsTheLaw <dimakrok@bk.ru>
2020-10-03 15:04:11 +0300

    ADD: first matr mult

--- AA_lab_02/src/main.kt
+++ AA_lab_02/src/main.kt
@@ -7,6 +7,7 @@ import kotlin.reflect.typeOf
 
 fun printOutMatrix(matrix: Array<IntArray>)
 {
+    print("\n") 
     for (i in matrix.indices)
     {
         for (j in matrix[i].indices)
@@ -15,9 +16,24 @@ fun printOutMatrix(matrix: Array<IntArray>)
     }
 }
 
+fun matricesMult(fMatrix: Array<IntArray>, sMatrix: Array<IntArray>) : Array<IntArray>
+{
+    val product = Array(fMatrix.size) { IntArray(sMatrix[0].size) }
+    for (i in fMatrix.indices)
+        for (j in sMatrix[0].indices)
+            for (k in fMatrix[0].indices)
+                product[i][j] += fMatrix[i][k] * sMatrix[k][j]
+    return product
+}
+
 fun main()
 {
     val firstMatrix = arrayOf(intArrayOf(3, -2, 5), intArrayOf(3, 0, 4))
+    val secondMatrix = arrayOf(intArrayOf(1, 1, 1), intArrayOf(3, 3, 3), intArrayOf(2, 2, 2))
     printOutMatrix(firstMatrix)
+    printOutMatrix(secondMatrix)
 
+    println("Result of multiplication")
+    val prod = matricesMult(firstMatrix, secondMatrix);
+    printOutMatrix(prod)
 }
