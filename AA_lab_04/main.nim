3c776fcf1330c4d40955c4901d27429c96bef04a
theSawIsTheLaw <dimakrok@bk.ru>
2020-10-21 20:13:19 +0300

    ADD: some of this parallel shit is working

--- AA_lab_04/main.nim
+++ AA_lab_04/main.nim
@@ -3,28 +3,26 @@ import random
 import strutils
 import sequtils
 
-type
-  Matrix[W, H: static[int]] =
-    array[1..W, array[1..H, int]]
+import locks
+
+const THREADS = 4
+
+var L : Lock
+initLock(L)
 
 proc printMat(mat : seq[seq[int]])=
     for i in 0..mat.len - 1:
         for j in 0..mat[1].len - 1:
-            stdout.write fmt"{mat[i][j]} "
+            stdout.write fmt"{mat[i][j]:5} "
         echo ""
-
-proc randMat(mat : var Matrix)=
-    for i in low(mat)..high(mat):
-        for j in low(mat[1])..high(mat[1]):
-            mat[i][j] = rand(-20..20)
 
 proc rowsComp(matrix : seq[seq[int]]) : seq[int]=
     var computedRows = newSeq[int](matrix.len)
 
     for i in 0..matrix.len - 1:
         var j = 0
-        while j < matrix[1].len - 1:
-            computedRows[j] += matrix[i][j] * matrix[i][j + 1]
+        while j < matrix[0].len - 1:
+            computedRows[i] += matrix[i][j] * matrix[i][j + 1]
             j += 2
 
     return computedRows
@@ -45,7 +43,9 @@ proc winogradMult(fMat : seq[seq[int]], sMat : seq[seq[int]]) : seq[seq[int]]=
         return
 
     var computedRows = rowsComp(fMat)
+    echo "Truly: ", computedRows
     var computedCols = colsComp(sMat)
+    echo "Truly: ", computedCols
 
     var product = newSeqWith(fMat.len, newSeq[int](sMat[0].len))
     for i in 0..product.len - 1:
@@ -98,22 +98,83 @@ proc getMat() : seq[seq[int]]=
 
     return mat
 
+proc rowsCompThreadFunc(info: tuple[startOfInterval, endOfInterval : int,
+                                    computedRows : ptr seq[int],
+                                    matrix : seq[seq[int]]]) {.thread.}=
+    acquire(L)
+    for i in info.startOfInterval..info.endOfInterval - 1:
+        var j = 0
+        while j < info.matrix[0].len - 1:
+            info.computedRows[i] += info.matrix[i][j] * info.matrix[i][j + 1]
+            j += 2
+    release(L)
+
+proc rowsCompParallel(matrix : seq[seq[int]]) : seq[int]=
+    var compRows = newSeq[int](matrix.len)
+    var compRowsPtr = addr compRows
+    var thr : array[0..THREADS, Thread[tuple[startOfInterval, endOfInterval: int, computedRows : ptr seq[int], matrix : seq[seq[int]]]]]
+    var size = (matrix.len / (THREADS + 1)).int
+    for i in 0..thr.len - 1:
+        createThread(thr[i], rowsCompThreadFunc, (i * size, (i + 1) * size, compRowsPtr, matrix))
+    joinThreads(thr)
+    return compRows
+
+proc colsCompThreadFunc(info : tuple[startOfInterval, endOfInterval : int,
+                        computedCols : ptr seq[int],
+                        matrix : seq[seq[int]]]) {.thread.}=
+    acquire(L)
+    var i = 0
+    while i < info.matrix.len - 1:
+        for j in info.startOfInterval..info.endOfInterval - 1:
+            info.computedCols[j] += info.matrix[i][j] * info.matrix[i + 1][j]
+        i += 2
+    release(L)
+
+proc colsCompParallel(matrix : seq[seq[int]]) : seq[int]=
+    var compCols = newSeq[int](matrix[0].len)
+    var compColsPtr = addr compCols
+    var thr : array[0..THREADS, Thread[tuple[startOfInterval, endOfInterval: int, computedCols : ptr seq[int], matrix : seq[seq[int]]]]]
+    var size = (matrix[0].len / THREADS).int
+    for i in 0..thr.len - 2:
+        createThread(thr[i], colsCompThreadFunc, (i * size, (i + 1) * size, compColsPtr, matrix))
+
+    createThread(thr[thr.len - 1], colsCompThreadFunc, ((thr.len - 1) * size, matrix[0].len, compColsPtr, matrix))
+
+    joinThreads(thr)
+    return compCols
+
+
+proc winogradMultParallel(fMat : seq[seq[int]], sMat : seq[seq[int]]) : seq[seq[int]]=
+    if (fMat[0].len != sMat.len):
+        return
+
+    var computedRows = rowsCompParallel(fMat)
+    echo computedRows
+    var computedCols = colsCOmpParallel(sMat)
+    echo computedCols
+
+    return fMat
+
+proc setRandomMat(matrix : var seq[seq[int]])=
+    for i in 0..matrix.len - 1:
+        for j in 0..matrix.len - 1:
+            matrix[i][j] = random(20)
+
 proc main()=
     var fMat = getMat()
-
     echo "\nGot matrix:"
     printMat(fMat)
     echo ""
 
     var sMat = getMat()
-
     echo "\nGot matrix:"
     printMat(sMat)
     echo ""
 
-    var prod = winogradMult(fMat, sMat)
+    var prod = winogradMultParallel(fMat, sMat)
+    var prod2 = winogradMult(fMat, sMat)
 
-    stdout.write "Result:\n"
-    printMat(prod)
+#    stdout.write "Result:\n"
+#    printMat(prod)
 
 main()
