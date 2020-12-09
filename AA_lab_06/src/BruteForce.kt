f8a507c9759f6a7b1e300c9f38de1aa3f9992edf
TheSawIsTheLaw <dimakrok@bk.ru>
2020-12-09 19:36:22 +0300

    ADD: BruteForce class realisation

--- AA_lab_06/src/BruteForce.kt
+++ AA_lab_06/src/BruteForce.kt
@@ -3,11 +3,19 @@ import java.util.*
 
 class BruteForce
 {
-    fun bruteForceAlg(graph: Graph) : MutableList<Pair<MutableList<Int>, Int>>
+    fun lenWay(graph: Graph, way : MutableList<Int>) : Int
     {
-        val bestWay = IntArray(graph.getSize())
+        var len = 0
+        for (i in 0 until way.size - 1)
+            len += graph.getWay(way[i], way[i + 1])
+
+        return len
+    }
+
+    fun bruteForceAlg(graph: Graph) : Pair<IntArray, Int>
+    {
+        var bestWay = IntArray(graph.getSize())
         var bestWayLen = -1
-        val emptyWay = mutableListOf<Int>()
         val queue : Queue<MutableList<Int>> = LinkedList<MutableList<Int>>()
         queue.add(mutableListOf(1, 0))
 
@@ -16,6 +24,25 @@ class BruteForce
             var currentWay = queue.peek()
             queue.remove()
             for (vertex in graph.getLinkedVertices(currentWay[currentWay.size - 1]))
+            {
+                if (currentWay.find { it == vertex } == currentWay.last())
+                {
+                    val newWay = currentWay
+                    newWay.add(vertex)
+                    if (newWay.size == graph.getSize())
+                    {
+                        val len = lenWay(graph, newWay)
+                        if (len < bestWayLen || bestWayLen > 0)
+                        {
+                            bestWayLen = len
+                            bestWay = newWay.toIntArray()
+                        }
+                    }
+                    else
+                        queue.add(newWay)
+                }
+            }
         }
+        return Pair(bestWay, bestWayLen)
     }
 }
