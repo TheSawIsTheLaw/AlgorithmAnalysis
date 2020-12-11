dac154e90da963126c7466539fe7fae279317cc8
theSawIsTheLaw <dimakrok@bk.ru>
2020-12-12 01:40:59 +0300

    ADD: commit to cafe

--- AA_lab_06/src/main.kt
+++ AA_lab_06/src/main.kt
@@ -2,23 +2,32 @@ import Graph
 import BruteForce
 import Colony
 import kotlin.random.Random
+import kotlin.system.measureNanoTime
 
 fun main()
 {
-    val graph = Graph(10)
+    val graph = Graph(4)
     graph.generate()
 
     graph.print()
 
-//    println("By the Brute Force Algorithm:")
-//    BruteForce().bruteForceAlg(graph)
+    println("By the Brute Force Algorithm:")
+//    var duration = measureNanoTime {
+    BruteForce().bruteForceAlg(graph)
+//    }
+//    println("${duration.toInt()}")
 
-    for (i in doubleArrayOf(0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0))
-    {
-        val ans2 = Colony(graph).antAlg(50, 1.0, 0.0, i, 2.0)
-        println("By the Ant Algorithm:")
-        println("Min way is: ${i} ${ans2}")
-    }
+//    for (i in doubleArrayOf(0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0))
+//    {
+//    duration = measureNanoTime {
+//        Colony(graph).antAlg(50, 0.5, 0.5, 0.5, 2.0)
+//    }
+//    println("${duration.toInt()}")
+
+    val ans2 = Colony(graph).antAlg(50, 0.5, 0.5, 0.5, 2.0)
+    println("By the Ant Algorithm:")
+    println("Min way is: $ans2")
+//    }
 
     return
 }
