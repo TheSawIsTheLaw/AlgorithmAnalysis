a531ebc696fe49a26774e2686f813b750c92de74
theSawIsTheLaw <dimakrok@bk.ru>
2020-12-14 01:26:57 +0300

    ADD: exaples of exec

--- AA_lab_07/src/main.kt
+++ AA_lab_07/src/main.kt
@@ -7,23 +7,34 @@ fun main()
     dict.fullByFile(directoryToCopySCV)
     if (dict.isEmpty())
         return
-    dict.print()
+//    dict.print()
 
-    println("For brutForce")
-    println(dict.getValueByBrutForce(460))
-    println(dict.getValueByBrutForce(6666))
-
-    println("For binarySearch")
     dict.sortForBinarySearch()
-    dict.getValueByBinarySearch(2)
-
-    println(dict.getValueByBinarySearch(800))
-    println(dict.getValueByBinarySearch(6666))
-
-    println("For modified segmented dictionary")
     dict.createSegmentedDictionary()
-    println(dict.getValueBySegmentedAndBinaryModified(4))
-    println(dict.getValueBySegmentedAndBinaryModified(460))
-    println(dict.getValueBySegmentedAndBinaryModified(800))
-    println(dict.getValueBySegmentedAndBinaryModified(-1))
+
+    println("Введите ключ для поиска:")
+    val key = readLine()!!.toInt()
+
+    println()
+    println("Результат поиска полным перебором: ${dict.getValueByBrutForce(key)}")
+
+    println("Результат бинарного поиска: ${dict.getValueByBinarySearch(key)}")
+
+    println("Результат поиска с испоьзованием сегментирования: ${dict.getValueBySegmentedAndBinaryModified(key)}")
+
+//    println("For brutForce")
+//    println(dict.getValueByBrutForce(460))
+//    println(dict.getValueByBrutForce(6666))
+
+//    println("For binarySearch")
+//    dict.getValueByBinarySearch(2)
+
+//    println(dict.getValueByBinarySearch(800))
+//    println(dict.getValueByBinarySearch(6666))
+
+//    println("For modified segmented dictionary")
+//    println(dict.getValueBySegmentedAndBinaryModified(4))
+//    println(dict.getValueBySegmentedAndBinaryModified(460))
+//    println(dict.getValueBySegmentedAndBinaryModified(800))
+//    println(dict.getValueBySegmentedAndBinaryModified(-1))
 }
