62ea8cbccd31ef55dd934944740640787d7a5c03
theSawIsTheLaw <dimakrok@bk.ru>
2020-12-13 16:09:49 +0300

    FIX: MutableMap to MutableList and shuffle

--- AA_lab_07/src/Dictionary.kt
+++ AA_lab_07/src/Dictionary.kt
@@ -5,17 +5,22 @@ import java.nio.file.Paths
 
 class Dictionary
 {
-    val dictionary: MutableMap<Int, String> = mutableMapOf()
+    val dictionary: MutableList<Pair<Int, String>> = mutableListOf()
 
     fun fullByFile(filePath: String)
     {
         val reader = Files.newBufferedReader(Paths.get(filePath))
         val parser = CSVParser(reader, CSVFormat.DEFAULT.withDelimiter(';'))
+
+        val shuffleList : MutableList<Pair<Int, String>> = mutableListOf()
+
         for (curRecord in parser)
-            dictionary[curRecord[0].toInt()] = curRecord[1]
+            dictionary.add(Pair(curRecord[0].toInt(), curRecord[1]))
 
         if (!parser.isClosed)
             parser.close()
+
+        dictionary.shuffle()
     }
 
     fun print()
